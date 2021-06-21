ed7319098a6a87d04ae6e3973c4f3af4fde40b32
godse823 <godserushikesh823@gmail.com>
2021-06-21 12:41:45 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import NavBar from './components/NavBar';
+import Cards from './components/Card/Cards';
+import axios from 'axios'
+{/*import { Switch, Link, Route } from 'react-router-dom';*/}
 
 function App() {
+
+  const initialData = {
+    loading: true,
+    datas: '',
+    err: ''
+  }
+  const [data,setData] = useState(initialData)
+
+  useEffect(()=>{
+    axios.get("https://api.rootnet.in/covid19-in/unofficial/covid19india.org/statewise")
+    .then(res=>{
+        setData({
+          loading:false,
+          datas:res.data,
+          err:''
+        })
+    })
+    .catch(err=>{
+      setData({
+        loading:false,
+        datas:'',
+        err:err
+      })
+    })
+  },[])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   {/*} <Link to="/">home</Link>
+    <Link to="/card">card</Link>
+    <Switch>
+      <Route exact path="/" component={NavBar}></Route>
+      <Route path="/card" component={()=><Cards data={data}/>}></Route>
+      
+      
+    </Switch>   */}
+    <NavBar/>
+    <Cards data={data}/>
+    
     </div>
   );
 }
